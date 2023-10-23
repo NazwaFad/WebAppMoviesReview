f156504a937e92fb5fa76cf43b2e67b4a52753c9
Nazwa Fadhil <111743890+NazwaFad@users.noreply.github.com>
2023-10-23 08:01:50 +0700

    backend commit

---
+++ src/main/java/shinecon/xx1/Movies/ReviewRepository.java
@@ -0,0 +1,10 @@
+package shinecon.xx1.Movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+
+}
---
+++ src/test/java/shinecon/xx1/Movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package shinecon.xx1.Movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
